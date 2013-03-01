fdf3e57b80fc9df827977c0d29488db6856eb10b
Pascal Duez <pascal@duez.me>
2013-03-01 16:13:37 +0100

    Run jshint...

--- js/flckrxplr.js
+++ js/flckrxplr.js
@@ -3,7 +3,7 @@
 
   "use strict";
 
-  document.addEventListener("DOMContentLoaded", function( e ) {
+  document.addEventListener("DOMContentLoaded", function() {
     document.body.classList.add("loading");
     var key = "ba40cdef3b366240ebebb25271a955fe";
     var src = "http://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key={key}&format=json";
@@ -19,12 +19,14 @@
         length = photos.length,
         i = 0;
 
+    function addLoaded( e ) {
+      e.target.classList.add("loaded");
+    }
+
     for ( ; i < length; i++ ) {
       var img = new Image();
       img.src = buildImgSrc( photos[i] );
-      img.addEventListener("load", function( e ) {
-        this.classList.add("loaded");
-      }, false);
+      img.addEventListener("load", addLoaded, false);
 
       var a = document.createElement("a");
       a.href = buildImgUrl( photos[i] );
@@ -44,7 +46,7 @@
 
     document.body.classList.remove("loading");
     document.body.classList.add("loaded");
-  }
+  };
 
   function buildImgSrc( photo ) {
     var str = "http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg";
