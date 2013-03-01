c2d87914fc2427091f7495f20b41a15d6b55a64c
Pascal Duez <pascal@duez.me>
2013-03-01 16:13:21 +0100

    Updates in Grunt config and tools

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,92 +1,47 @@
-/*global module:false*/
-module.exports = function(grunt) {
+"use strict";
 
+var path = require("path");
+var lrSnippet = require("grunt-contrib-livereload/lib/utils").livereloadSnippet;
+var folderMount = function folderMount( connect, point ) {
+  return connect.static( path.resolve(point) );
+};
+
+module.exports = function( grunt ) {
   // Project configuration.
   grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['lib/<%= pkg.name %>.js'],
-        dest: 'dist/<%= pkg.name %>.js'
+    connect: {
+      livereload: {
+        options: {
+          port: 9000,
+          middleware: function( connect ) {
+            return [ lrSnippet, folderMount(connect, ".") ];
+          }
+        }
       }
     },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
+    regarde: {
+      fred: {
+        files: ["*.html", "js/*.js", "css/*.css", "img/*.{png,jpg,jpeg,svg}"],
+        tasks: ["livereload"]
       }
     },
     jshint: {
       options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        }
+        jshintrc: ".jshintrc"
       },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
-      }
-    },
-    connect: {
-      server: {
-        options: {
-          port: 8888,
-          keepalive: true
-        }
-      }
+      all: [
+        "Gruntfile.js",
+        "js/*.js"
+      ]
     }
   });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks("grunt-regarde");
+  grunt.loadNpmTasks("grunt-contrib-connect");
+  grunt.loadNpmTasks("grunt-contrib-livereload");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask("default", "jshint");
 
+  grunt.registerTask("server", ["livereload-start", "connect", "regarde"]);
 };
